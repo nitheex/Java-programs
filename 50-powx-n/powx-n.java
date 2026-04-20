de8c89d8dbacd61128536ab5ba1e3785644ef0de
NITHEESHKUMAR M <nitheesh638331@gmail.com>
2026-04-20 14:38:47 +0530

    Time: 1 ms (10.14%) | Memory: 47.8 MB (73.18%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long num = n;
+        if (num < 0) {
+            x = 1 / x;
+            num = -num;
+        }
+
+        double result = 1;
+        double currentProduct = x;
+
+        while (num > 0) {
+            if (num % 2 == 1) {
+                result = result * currentProduct;
+            }
+            currentProduct = currentProduct * currentProduct;
+            num = num / 2;
+        }
+
+        return result;
+    }
+}
