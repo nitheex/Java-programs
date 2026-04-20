8f1d18659948322a4f561bbbdffa83595a4b49f2
NITHEESHKUMAR M <nitheesh638331@gmail.com>
2026-04-20 14:37:10 +0530

    Time: 7 ms (74.29%) | Memory: 49.5 MB (75.33%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            map.get(sortedWord).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
